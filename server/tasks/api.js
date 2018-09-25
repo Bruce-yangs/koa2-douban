023d109268653d267cf34665e074d529cf2353cb
yangkun <yangkun@leying365.com>
2018-09-25 18:43:15 +0800

    first

---
+++ server/tasks/api.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Administrator on 2018/9/21.
+ * @author yangkun
+ */
+// http://api.douban.com/v2/movie/subject/1764796
+  const rp = require('require-promise-native');
+async function fetchMovie(item) {
+  const url = `http://api.douban.com/v2/movie/subject/${item.doubanId}`;
+  return res = await rp(url);
+}
+;(async () => {
+    let movies = [
+      {
+        doubanId: 1292213,
+        title: '大话西游之大圣娶亲',
+        rate: 9.2,
+        poster: 'https://img3.doubanio.com/view/photo/l_ratio_poster/public/p2455050536.jpg'
+      },
+      {
+        doubanId: 1851857,
+        title: '蝙蝠侠：黑暗骑士',
+        rate: 9.1,
+        poster: 'https://img3.doubanio.com/view/photo/l_ratio_poster/public/p462657443.jpg'
+      }
+    ];
+    movies.map(async movie => {
+      let movieData = await fetchMovie(movie)
+    })
+  })();
